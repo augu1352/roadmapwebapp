b8fe679603e83493a3a0887443d59fdbfec00b33
August Emmery-Funch <augustemmeryfunch@gmail.com>
2019-06-27 12:42:24 +0200

    update

--- roadmapper/roadmapper/urls.py
+++ roadmapper/roadmapper/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('roadmap/', include('roadmap.urls')),
     path('workflow/', include('workflow.urls'))
 ]
