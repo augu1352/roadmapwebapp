dde0c3a2611527a759f4167e6898fd32460a4f98
August Emmery-Funch <augustemmeryfunch@gmail.com>
2019-06-29 00:26:46 +0200

    Update settings.py

--- roadmapper/roadmapper/settings.py
+++ roadmapper/roadmapper/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&&(okn565)@nud^an6@$34gu)_!1sm#%w#)-@q^4u=443szk0g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
