2aefc8939c56cbadc4b397b3dfa76d7e7cff5a7e
August Emmery-Funch <augustemmeryfunch@gmail.com>
2019-06-28 20:39:58 +0200

    Update urls.py

--- roadmapper/roadmap/urls.py
+++ roadmapper/roadmap/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("roadmap/", views.roadmap, name="roadmap"),
     path("login/", views.login, name="login"),
-    path("createuser", views.createUser, name="createUser"),
+    path("createuser/", views.createUser, name="createUser"),
     ]
