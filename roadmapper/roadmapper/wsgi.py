f52b51ce3060b4c92719cf12a8ddd05d8031ce07
August Emmery-Funch <augustemmeryfunch@gmail.com>
2019-06-29 00:29:38 +0200

    Update wsgi.py

--- roadmapper/roadmapper/wsgi.py
+++ roadmapper/roadmapper/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append("/home/augu1352/roadmapRepo/roadmapwebapp/roadmapper/")
 
 from django.core.wsgi import get_wsgi_application
 
